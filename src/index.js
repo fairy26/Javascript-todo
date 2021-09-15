e8f8f8c630734a3928cd1de8ab6aff848fbce95b
fairy26 <56621254+fairy26@users.noreply.github.com>
2021-09-15 15:58:48 +0900

    35. タスクの完了機能

--- src/index.js
+++ src/index.js
@@ -4,47 +4,68 @@ const onClickAdd = () => {
   // テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  // alert();
 
   // divタグ生成
   const div = document.createElement("div");
   div.className = "list-row";
-  // console.log(div);
 
   // liタグ生成
   const li = document.createElement("li");
   li.innerText = inputText;
-  // console.log(li);
 
   // button(完了)タグの生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
-  // console.log(completeButton);
   completeButton.addEventListener("click", () => {
-    // alert("完了");
+    // 押された完了ボタンの親タグ(div)を未完了リストから削除
+    deleteFromIncompleteList(completeButton.parentNode);
+
+    // 完了リストに追加する要素
+    const addTarget = completeButton.parentNode;
+
+    // TODO内容テキストを取得
+    const text = addTarget.firstElementChild.innerText;
+
+    // div以下を初期化
+    addTarget.textContent = null;
+
+    // liタグ生成
+    const li = document.createElement("li");
+    li.innerText = text;
+    console.log(li);
+
+    //buttonタグ生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    // divタグの子要素に各要素を設定
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    // 完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   // button(削除)タグの生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
-  // console.log(deleteButton);
   deleteButton.addEventListener("click", () => {
-    // alert("削除");
-
     // 押された削除ボタンの親タグ(div)を未完了リストから削除
-    const deleteTarget = deleteButton.parentNode;
-    // console.log(deleteTarget);
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
+    deleteFromIncompleteList(deleteButton.parentNode);
   });
 
   // divタグの子要素に各要素を設定
   div.appendChild(li);
   div.appendChild(completeButton);
   div.appendChild(deleteButton);
-  // console.log(div);
 
   // 未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
+};
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
